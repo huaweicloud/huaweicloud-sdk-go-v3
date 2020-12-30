b4fbbaa7c44c0d3133c01bf54964f5e247090f1a
wuchen <wuchen25@huawei.com>
2020-12-30 13:19:28 +0800

    modify dcs models

--- services/dcs/v2/model/model_hotkeys_body.go
+++ services/dcs/v2/model/model_hotkeys_body.go
@@ -10,8 +10,9 @@ package model
 import (
 	"encoding/json"
 	"errors"
-	"github.com/huaweicloud/huaweicloud-sdk-go-v3/core/converter"
 	"strings"
+
+	"github.com/huaweicloud/huaweicloud-sdk-go-v3/core/converter"
 )
 
 // 热key记录结构体
@@ -20,10 +21,10 @@ type HotkeysBody struct {
 	Name *string `json:"name,omitempty"`
 	// key类型
 	Type *HotkeysBodyType `json:"type,omitempty"`
-	// 大key所在的分片，仅在实例类型为集群时支持,格式为ip:port
+	// 热key所在的分片，仅在实例类型为集群时支持,格式为ip:port
 	Shard *string `json:"shard,omitempty"`
-	// 大key所在的db
-	Db *string `json:"db,omitempty"`
+	// 热key所在的db
+	Db *int32 `json:"db,omitempty"`
 	// key的value大小。
 	Size *int32 `json:"size,omitempty"`
 	// key大小的单位。type为string时，单位是：byte；type为list/set/zset/hash时，单位是：count
